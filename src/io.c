d7721009f268ad9054ec698f4b3a80d231a9b35c
playduck <robin.prillwitz@gmail.com>
2019-12-05 13:06:02 +0100

    print spielfeld von master hinzugefügt

--- src/io.c
+++ src/io.c
@@ -9,14 +9,57 @@ void print_spielfeld(spiel_t spiel)
 {
     clear_screen();
 
-    // TODO improve
-    for(int i = 0; i < FELD_GROESSE; i++)   {
-        for(int j = 0; j < FELD_GROESSE; j++)   {
-            printf("%d ", spiel.spielfeld[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n\n");
+	int feld_zeile = 0;
+	int feld_spalte = 0;
+
+	for(int zeilen = 1; zeilen <= 5; zeilen++)
+	{
+		if(zeilen % 2 == 0) // jede zweite zeile ist eine trennlinie
+		{
+			printf("---|---|---");
+		}
+		else
+		{
+			for(int spalten = 1; spalten <= 5; spalten++)
+			{
+				char temp;
+
+				if(spalten % 2 == 0) //jeder zweite brocken ist ein |
+				{
+					printf("|");
+				}
+				else
+				{
+					switch(spiel.spielfeld[feld_zeile][feld_spalte]) //ob x oder o oder nichts
+					{
+						case leer:
+							temp = ' ';
+							break;
+
+						case kreuz:
+							temp = 'x';
+							break;
+
+						case kreis:
+							temp = 'o';
+							break;
+					}
+
+					feld_spalte++;
+					printf(" %c ", temp);
+				}
+				
+				if(spalten == 5)
+				{
+					feld_zeile++;
+					feld_spalte = 0;
+				}
+			}
+		}
+
+		printf("\n");
+	}
+
 }
 
 void print_ergebnis(ergebnis_t ergebnis)
