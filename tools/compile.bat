853ecdf2860e298a4c6016fd6164e6037933015e
playduck <robin.prillwitz@gmail.com>
2019-12-06 11:35:25 +0100

    updated compile scripts

--- tools/compile.bat
+++ tools/compile.bat
@@ -1,3 +1,3 @@
 @echo off
 set OBJ=./src/bot.c ./src/io.c ./src/main.c ./src/spiel.c ./src/turnier.c
-gcc -o tic_tac_toe %OBJ% -I ../includes/*
+gcc -Wall -std='c99' -Wextra -O1 -o tic_tac_toe %OBJ% -I ../includes/*
--- tools/compile.sh
+++ tools/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 OBJ="./src/bot.c ./src/io.c ./src/main.c ./src/spiel.c ./src/turnier.c"
-gcc -o tic_tac_toe.o ${OBJ} -I ../includes/*
+gcc -Wall -std='c99' -Wextra -O1 -o tic_tac_toe.o ${OBJ} -I ../includes/*
